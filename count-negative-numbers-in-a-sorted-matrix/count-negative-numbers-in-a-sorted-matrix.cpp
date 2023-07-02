223f8f91fb768373031fa8ae6c0a2a19ce6a86c1
Debjit Karmakar <karmadebjit@gmail.com>
2023-07-02 11:49:24 +0530

    Time: 7 ms (99.82%) | Memory: 10.4 MB (60.05%) - LeetSync

---
+++ count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& v) {
+        int row=v.size(),col=v[0].size();
+        int r=0,c=col-1,ans=0;
+
+        while(r<row && c>=0)
+        {
+            int x=v[r][c];
+            if(x<0)
+            {
+                ans+=(row-r);
+                c--;
+            }
+            else
+            r++;
+        }
+        return ans;
+    }
+};
