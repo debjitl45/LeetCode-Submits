e6a6cb29eaf1e46c21f05c2feb7fa51723f3b0c5
Debjit Karmakar <karmadebjit@gmail.com>
2023-07-07 17:02:19 +0530

    Time: 34 ms (38.80%) | Memory: 9.9 MB (95.61%) - LeetSync

--- maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.cpp
+++ maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.cpp
@@ -1,46 +1,44 @@
 class Solution {
 public:
     int maxConsecutiveAnswers(string answerKey, int k) {
-        int n = answerKey.length();
-        int left = k, right = n;
-        
-        while (left < right) {
-            int mid = (left + right + 1) / 2;
-            
-            if (isValid(answerKey, mid, k)) {
-                left = mid;
-            } else {
-                right = mid - 1;
+        int count=0;
+        int left=0;
+        int ans=INT_MIN;
+        for(int i=0;i<answerKey.size();i++)
+        {
+            if(answerKey[i]=='F')
+            {
+                count++;
             }
-        }
-        
-        return left;
-    }
-    
-    bool isValid(string answerKey, int size, int k) {
-        int n = answerKey.length();
-        unordered_map<char, int> counter;
-        
-        for (int i = 0; i < size; i++) {
-            char c = answerKey[i];
-            counter[c]++;
-        }
-        
-        if (min(counter['T'], counter['F']) <= k) {
-            return true;
-        }
-        
-        for (int i = size; i < n; i++) {
-            char c1 = answerKey[i];
-            counter[c1]++;
-            char c2 = answerKey[i - size];
-            counter[c2]--;
-            
-            if (min(counter['T'], counter['F']) <= k) {
-                return true;
+            while(count>k)
+            {
+                if(answerKey[left]=='F')
+                {
+                    count--;
+                }
+                left++;
             }
+            ans=max(ans,i-left+1);
         }
-        
-        return false;
+
+        count=0;
+        left=0;
+        for(int i=0;i<answerKey.size();i++)
+        {
+            if(answerKey[i]=='T')
+            {
+                count++;
+            }
+            while(count>k)
+            {
+                if(answerKey[left]=='T')
+                {
+                    count--;
+                }
+                left++;
+            }
+            ans=max(ans,i-left+1);
+        }
+        return ans;
     }
 };
