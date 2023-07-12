1591bba234d7622853a8c5aed262a83774e72bc5
Debjit Karmakar <karmadebjit@gmail.com>
2023-07-12 08:10:16 +0530

    Time: 205 ms (55.02%) | Memory: 51.7 MB (55.20%) - LeetSync

---
+++ find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool dfs(vector<vector<int>>& graph,int src,vector<bool>& visited,vector<bool>& dfsVisited,set<int>& cycle)
+    {
+        visited[src]=true;
+        dfsVisited[src]=true;
+
+        for(int i=0;i<graph[src].size();i++)
+        {
+            int child=graph[src][i];
+            if(!visited[child])
+            {
+                if(dfs(graph,child,visited,dfsVisited,cycle))
+                {
+                    cycle.insert(child);
+                    return true;
+                }
+            }
+                else
+                {
+                    if(dfsVisited[child])
+                    {
+                        cycle.insert(child);
+                        return true;
+                    }
+                }
+        }
+        dfsVisited[src]=false;
+        return false;
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<bool> visited(n,false),dfsVisited(n,false);
+        set<int> cycle;
+        for(int i=0;i<n;i++)
+        {
+            if(!visited[i])
+            {
+                if(dfs(graph,i,visited,dfsVisited,cycle))
+                cycle.insert(i);
+            }
+        }
+        vector<int> ans;
+        for(int i=0;i<n;i++)
+        {
+            if(cycle.find(i)==cycle.end()) ans.push_back(i);
+        }
+        return ans;
+    }
+};
