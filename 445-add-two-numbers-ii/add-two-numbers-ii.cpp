dcc37cf7f783ad2254dbbad655802126d0ab91bc
Debjit Karmakar <karmadebjit@gmail.com>
2023-07-17 07:20:45 +0530

    Time: 35 ms (78.21%) | Memory: 73.8 MB (14.05%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.cpp
+++ 445-add-two-numbers-ii/add-two-numbers-ii.cpp
@@ -1,59 +1,38 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    
-    ListNode* reverse(ListNode* head){
-        ListNode *p, *q;
-        p=head, q=NULL;
-        
-        while(p!=NULL){
-            ListNode* on=p->next; // keep track of original next node
-            p->next=q; // reversing links
-            q=p;
-            p=on;
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        stack<int> s1, s2;
+
+        while (l1 != nullptr) {
+            s1.push(l1->val);
+            l1 = l1->next;
         }
-        return q;
-    }
-    
-    ListNode* addTwoNumbers(ListNode* head1, ListNode* head2) {
-        ListNode* p=reverse(head1);
-        ListNode* q=reverse(head2);
-        
-        ListNode *head, *last;
-        head=last=new ListNode(-1); // pointers to new list
-        
-        int carry=0;
-        while(p!=NULL || q!=NULL){
-            int d=(p!=NULL ? p->val:0) + (q!=NULL ? q->val:0) + carry;
-            
-            ListNode *temp=new ListNode(d%10);
-            last->next=temp;
-            last=temp;
-            
-            carry=d/10;
-            
-            if(p) p=p->next;
-            if(q) q=q->next;
+
+        while (l2 != nullptr) {
+            s2.push(l2->val);
+            l2 = l2->next;
         }
-        
-        if(carry!=0){
-            ListNode *temp=new ListNode(carry);
-            last->next=temp;
-            last=temp;
+
+        int totalSum = 0, carry = 0;
+        ListNode* ans = new ListNode();
+        while (!s1.empty() || !s2.empty()) {
+            if (!s1.empty()) {
+                totalSum += s1.top();
+                s1.pop();
+            }
+            if (!s2.empty()) {
+                totalSum += s2.top();
+                s2.pop();
+            }
+
+            ans->val = totalSum % 10;
+            carry = totalSum / 10;
+            ListNode* newNode = new ListNode(carry);
+            newNode->next = ans;
+            ans = newNode;
+            totalSum = carry;
         }
-        
-        head=head->next; // since head was pointing to dummy node
-        head=reverse(head);
-        
-        return head;
+
+        return carry == 0 ? ans->next : ans;
     }
 };
