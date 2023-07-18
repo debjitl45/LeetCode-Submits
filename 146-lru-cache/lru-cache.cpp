0a5779dbc916fbc00804d90d9b6f026fd2deb8e3
Debjit Karmakar <karmadebjit@gmail.com>
2023-07-18 07:24:48 +0530

    Time: 419 ms (99.69%) | Memory: 164.3 MB (95.94%) - LeetSync

---
+++ 146-lru-cache/lru-cache.cpp
@@ -0,0 +1,42 @@
+class LRUCache {
+public:
+    list<pair<int,int>> l;
+    unordered_map<int,list<pair<int,int>>::iterator> mp;
+    int sz;
+    LRUCache(int capacity) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        sz=capacity;
+    }
+    
+    int get(int key) {
+        if(mp.find(key)==mp.end())
+        return -1;
+        l.splice(l.begin(),l,mp[key]);
+        return mp[key]->second;
+    }
+    
+    void put(int key, int value) {
+        if(mp.find(key)!=mp.end())
+        {
+            l.splice(l.begin(),l,mp[key]);
+            mp[key]->second=value;
+            return;
+        }
+        if(l.size()==sz)
+        {
+            auto d_key=l.back().first;
+            l.pop_back();
+            mp.erase(d_key);
+        }
+        l.push_front({key,value});
+        mp[key]=l.begin();
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
