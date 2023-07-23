5af195141e5fab96e700a81b5ce86a5099ba1dba
Debjit Karmakar <karmadebjit@gmail.com>
2023-07-23 21:59:20 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (21.79%) - LeetSync

---
+++ 1005-univalued-binary-tree/univalued-binary-tree.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    set<int> st;
+    void preorder(TreeNode* root)
+    {
+        if(!root) return;
+
+        st.insert(root->val);
+        preorder(root->left);
+        preorder(root->right);
+    }
+    bool isUnivalTree(TreeNode* root) {
+        preorder(root);
+        if(st.size()>1)
+        return false;
+        
+        return true;
+    }
+};
