f9926cb41138e16ab66e0b063601369621d6af5a
Debjit Karmakar <karmadebjit@gmail.com>
2023-07-24 20:30:46 +0530

    Time: 33 ms (57.14%) | Memory: 39.1 MB (14.29%) - LeetSync

---
+++ 2881-split-strings-by-separator/split-strings-by-separator.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> splitWordsBySeparator(vector<string>& w, char sep) {
+        vector<string> ans;
+        for(int i=0;i<w.size();i++)
+        {
+            string s=w[i];
+            string temp="";
+            for(auto a:s)
+            {
+                if(a==sep && temp!="")
+                {
+                ans.push_back(temp);
+                temp="";
+                }
+                if(a!=sep)
+                temp+=a;
+            }
+            if(temp!="") ans.push_back(temp);
+        }
+        return ans;
+    }
+};
