ea46441cb1ae159aa537e9a7a1b826c493c5f772
Debjit Karmakar <karmadebjit@gmail.com>
2023-07-28 23:53:21 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (35.73%) - LeetSync

---
+++ 2659-number-of-even-and-odd-bits/number-of-even-and-odd-bits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> evenOddBit(int n) {
+        int e=0,o=0;
+        for(int i=0;n>0;i++)
+        {
+            if(n%2==1)
+            {
+                if(i%2==0)
+                e++;
+                else
+                o++;
+            }
+            n=n/2;
+        }
+        return {e,o};
+    }
+};
