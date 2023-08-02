16d333f071add4c33a42bd56dd071ad16dcfc47c
Debjit Karmakar <karmadebjit@gmail.com>
2023-08-02 07:34:55 +0530

    Time: 2 ms (68.70%) | Memory: 7.7 MB (60.28%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,22 +1,22 @@
 class Solution {
     vector<vector<int>> ans;
     
-    void recursion(int n, vector<int> &comb, vector<int>& nums){
-        if(n == 0)
-            ans.push_back(comb);
-        
-        for(int i = 0; i < n; i++){
-            int temp = nums[i];
-            
-            comb.push_back(temp);
-            nums.erase(nums.begin() + i);
-			
-            recursion(n-1, comb, nums);
-			
-            nums.insert(nums.begin()+i, temp);
+    void recursion(int n, vector<int> &comb, vector<int>& nums)
+    {
+        if(n==0)
+        ans.push_back(comb);
+
+        for(int i=0;i<n;i++)
+        {
+            int x=nums[i];
+            comb.push_back(x);
+            nums.erase(nums.begin()+i);
+            recursion(n-1,comb,nums);
+            nums.insert(nums.begin()+i,x);
             comb.pop_back();
         }
     }
+
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<int> comb;
