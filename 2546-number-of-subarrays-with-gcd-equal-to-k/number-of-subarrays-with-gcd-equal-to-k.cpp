645e5a02446dd2d44be58a29ac75d509f4b29d47
Debjit Karmakar <karmadebjit@gmail.com>
2023-08-08 07:19:49 +0530

    Time: 13 ms (79.50%) | Memory: 9 MB (8.20%) - LeetSync

--- 2546-number-of-subarrays-with-gcd-equal-to-k/number-of-subarrays-with-gcd-equal-to-k.cpp
+++ 2546-number-of-subarrays-with-gcd-equal-to-k/number-of-subarrays-with-gcd-equal-to-k.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    int subarrayGCD(vector<int>& nums, int k) {
-        int ans=0;
-		// check for all size 1 subarray
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==k) ans++;
+    int subarrayGCD(vector<int>& v, int k) {
+        int ans=0,n=v.size();
+        for(int i=0;i<n;i++)
+        {
+            if(v[i]==k)
+            ans++;
         }
-		// check for greater then 1 size subarray
-        for(int i=0;i<nums.size();i++){
-            bool flg = false;
-            int gcd = nums[i];
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[j]%k!=0) break;
-                gcd = __gcd(gcd,nums[j]);
-                if(gcd==k) flg = true;
-                if(flg) ans+=1;
-                // cout<<nums[i]<<" ";
+        for(int i=0;i<n;i++)
+        {
+            int hcf=v[i];
+            for(int j=i+1;j<n;j++)
+            {
+                if(v[j]%k!=0) break;
+                hcf=__gcd(hcf,v[j]);
+                if(hcf==k)
+                ans++;
             }
-            // cout<<"\n";
         }
         return ans;
     }
