9e3ec5aedc39208802c50b171a53df1dad0087dd
Debjit Karmakar <karmadebjit@gmail.com>
2023-08-13 09:09:31 +0530

    Time: 130 ms (88.00%) | Memory: 92.9 MB (22.46%) - LeetSync

--- 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
-    bool solve(vector<int>& nums, int i, vector<int>& dp){
-        if(i == nums.size()) return true;
-        if(dp[i] != -1) return dp[i];
-        
-        if(i + 1 < nums.size() && nums[i] == nums[i+1] ) {
-            if(solve(nums, i + 2, dp)) return true;
-            if(i + 2 < nums.size() && nums[i] == nums[i+2]){
-                if(solve(nums, i + 3, dp)) return true;
-            }
+    bool solve(vector<int>& v, int i, vector<int>& dp){
+        if(i==v.size()) return true;
+        if(dp[i]!=-1) return dp[i];
+
+        if(i+1<v.size() && v[i]==v[i+1])
+        {
+             if(solve(v,i+2,dp))
+             return true;
         }
-        if(i + 2 < nums.size() && nums[i] == nums[i+1]-1 && nums[i] == nums[i+2] - 2 ){
-            if(solve(nums, i + 3, dp)) return true;
+        if(i+2<v.size() && v[i]==v[i+1] && v[i]==v[i+2])
+        {
+             if(solve(v,i+3,dp))
+             return true;
         }
-        return dp[i] = false;
+        if(i+2<v.size() && v[i]==v[i+1]-1 && v[i]==v[i+2]-2)
+        {
+             if(solve(v,i+3,dp))
+             return true;
+        }
+       return dp[i]=false;
     }
     bool validPartition(vector<int>& nums) {
          vector<int> dp(nums.size(), -1);
