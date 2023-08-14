44067ed3227dad91c5f64383d8962ec36e15e915
Debjit Karmakar <karmadebjit@gmail.com>
2023-08-14 21:02:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (70.19%) - LeetSync

--- 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.cpp
@@ -3,22 +3,19 @@ public:
     int minimumOperations(vector<int>& v) {
         priority_queue<int,vector<int>,greater<int>> pq;
         int n=v.size();
-        for(int i=0;i<n;i++)
+        for(int x:v)
         {
-            pq.push(v[i]);
+            pq.push(x);
         }
         int ans=0,num=0;
         while(pq.size()>0)
         {
-            if(pq.top()==0 || pq.top()-num==0)
-            {
-                pq.pop();
-            }
+            if(pq.top()==0 || pq.top()-num==0) pq.pop();
+
             else
             {
                 int temp=pq.top()-num;
                 num+=temp;
-                pq.pop();
                 ans++;
             }
         }
