8566e07a29c1f454920eaf423e557fdf5140e504
Debjit Karmakar <karmadebjit@gmail.com>
2023-08-24 15:11:44 +0530

    Time: 0 ms (100.00%) | Memory: 6 MB (56.23%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.cpp
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -2,18 +2,15 @@ class Solution {
 public:
     int maximum69Number (int n) {
         string s=to_string(n);
-        string t;
         int cnt=0;
         for(int i=0;i<s.size();i++)
         {
             if(s[i]=='6' && cnt<1)
             {
-                t.push_back('9');
+                s[i]='9';
                 cnt++;
             }
-            else
-            t.push_back(s[i]);
         }
-        return stoi(t);
+        return stoi(s);
     }
 };
