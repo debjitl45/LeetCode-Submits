4c25a36a088c9b476588efb5d98ee4ea2f791655
Debjit Karmakar <karmadebjit@gmail.com>
2023-08-28 01:20:34 +0530

    Time: 11 ms (77.96%) | Memory: 15.8 MB (55.79%) - LeetSync

--- 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.cpp
+++ 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int minOperations(vector<int>& v) {
-        int n=v.size(),bar=v[0],ans=0;
+        int n=v.size(),ans=0,bar=v[0];
         for(int i=1;i<n;i++)
         {
             if(bar>=v[i])
