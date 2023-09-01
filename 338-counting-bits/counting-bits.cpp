3bcae89ebff79d85cac97ad24c160b89d6830eae
Debjit Karmakar <karmadebjit@gmail.com>
2023-09-01 07:22:56 +0530

    Time: 11 ms (11.73%) | Memory: 8.5 MB (14.67%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
+    int counter(int n)
+    {
+        if(n<=1) return n;
+
+        if(n%2==0) return counter(n/2);
+        else  return 1+counter(n/2);
+    }
     vector<int> countBits(int n) {
         vector<int> ans;
         for(int i=0;i<=n;i++)
         {
-            ans.push_back(__builtin_popcount(i));
+            ans.push_back(counter(i));
         }
         return ans;
     }
