185ecf30892f61285afb0e7f5be27da3649b2e22
Debjit Karmakar <karmadebjit@gmail.com>
2023-09-01 07:18:26 +0530

    Time: 3 ms (89.82%) | Memory: 8.5 MB (14.67%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -4,16 +4,8 @@ public:
         vector<int> ans;
         for(int i=0;i<=n;i++)
         {
-            ans.push_back(counter(i));
+            ans.push_back(__builtin_popcount(i));
         }
         return ans;
-    }
-    int counter(int n)
-    {
-        if(n==0) return 0;
-        if(n==1) return 1;
-
-        if(n%2==0) return counter(n/2);
-        else return 1+counter(n/2);
     }
 };
