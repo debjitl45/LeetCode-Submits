f25cf3c16b4d3a0822289fabddf1523c9cccf38a
Debjit Karmakar <karmadebjit@gmail.com>
2023-09-07 18:27:03 +0530

    Time: 7 ms (27.79%) | Memory: 8.7 MB (6.64%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -3,9 +3,10 @@ public:
     int counter(int n)
     {
         if(n<=1) return n;
-
-        if(n%2==0) return counter(n/2);
-        else  return 1+counter(n/2);
+        if(n%2==0)
+        return counter(n/2);
+        else
+        return 1+counter(n/2);
     }
     vector<int> countBits(int n) {
         vector<int> ans;
